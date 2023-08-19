bf614ffb510221d23ee24a9270b38dc913444aa7
Ricard <ricardbarnes@gmail.com>
2023-08-19 18:15:49 +0200

    initial commit

--- api/src/main/kotlin/cat/vonblum/mamazone/api/controller/CustomerGrpcController.kt
+++ api/src/main/kotlin/cat/vonblum/mamazone/api/controller/CustomerGrpcController.kt
@@ -1,11 +1,9 @@
 package cat.vonblum.mamazone.api.controller
 
-import cat.vonblum.mamazone.api.CustomerOuterClass
 import cat.vonblum.mamazone.api.CustomerServiceGrpc
 import cat.vonblum.mamazone.api.mapper.CustomerBusGrpcMapper
 import cat.vonblum.mamazone.shared.domain.bus.command.CommandBus
 import cat.vonblum.mamazone.shared.domain.bus.query.QueryBus
-import io.grpc.stub.StreamObserver
 
 class CustomerGrpcController(
     private val commandBus: CommandBus,
